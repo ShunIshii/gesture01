654dfcb50d17852746cafb28d7868aca6db6e253
Shun Ishii <39784917+ShunIshii@users.noreply.github.com>
2019-04-15 09:44:25 +0900

    just_accelerater
    only plot figures to serial monitor

--- accelerater.ino
+++ accelerater.ino
@@ -1,64 +1,54 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiUdp.h>
 #include <SparkFunLSM9DS1.h>
-#define ADAddr 0x48
+
+#define LSM9DS1_M 0x1E
+#define LSM9DS1_AG 0x6B
 
 LSM9DS1 imu;
-void printAccel(); // Print "A: ax, ay, az"
 
-const char * networkName = "000D0BB8E766";
-const char * networkPswd = "";
-const char * udpAddress = "";
-const int udpPort = 3333;
-boolean connected= false;
-
-WiFiUDP udp;
-
-float x,y,z;//加速度
-float norm;//加速度の大きさ
-int frame;//フレーム数
-int readFrame=0;
-boolean FrameFlg=false;//ジェスチャ認識開始時からフレーム更新
- //------------------------------------------------------------------------
-#define LSM9DS1_M 0x1E  // SPIアドレス設定 0x1C if SDO_M is LOW
-#define LSM9DS1_AG 0x6B // SPIアドレス設定 if SDO_AG is LOW
+float x,y,z;
 uint16_t connectionState = 0;
 
-//----------------------　setup　---------------------------
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    imu.settings.device.commInterface = IMU_MODE_I2C;
-    imu.settings.device.mAddress = LSM9DS1_M;
-    imu.settings.device.agAddress = LSM9DS1_AG;
-    if (connectionState == 0)
+  imu.settings.device.commInterface = IMU_MODE_I2C;
+  imu.settings.device.mAddress = LSM9DS1_M;
+  imu.settings.device.agAddress = LSM9DS1_AG;
+  if (connectionState == 0)
+  {
+    connectionState = imu.begin();
+    while (connectionState == 0)
     {
-        connectionState = imu.begin();
-        while (connectionState == 0)
-        {
-            Serial.println("Failed to communicate with LSM9DS1.");
-            Serial.println("Double-check wiring.");
-            Serial.println("Default settings in this sketch will "
-                           "work for an out of the box LSM9DS1 "
-                           "Breakout, but may need to be modified "
-                           "if the board jumpers are.");
-            Serial.print("Connection Status: ");
-            Serial.println(imu.begin());
-            delay(1000);
-            connectionState = imu.begin();
-            Serial.println("---------------------------------------------------------\n");
-        }if(connectionState!=1){
-          Serial.print("connectionState: ");
-          Serial.println(connectionState);
-        }
+      Serial.println("Failed to communicate with LSM9DS1.");
+      Serial.println("Double-check wiring.");
+      Serial.println("Default settings in this sketch will "
+                     "work for an out of the box LSM9DS1 "
+                     "Breakout, but may need to be modified "
+                     "if the board jumpers are.");
+      Serial.print("Connection Status: ");
+      Serial.println(imu.begin());
+      delay(1000);
+      connectionState = imu.begin();
+      Serial.println("------------------------------------------------------\n");
+    }if(connectionState!=1){
+      Serial.print("connectionState: ");
+      Serial.println(connectionState);
     }
-    frame=0;
-    //connectToWiFi(networkName, networkPswd);
+  }
 }
 
 
-void loop() {
-  //imu.readAccel();
+void loop()
+{
+  imu.readAccel();
+  x=imu.calcAccel(imu.ax)*10;
+  y=imu.calcAccel(imu.ay)*10;
+  z=imu.calcAccel(imu.az)*10;
+  Serial.print("x=");
+  Serial.print(x);
+  Serial.print(", y=");
+  Serial.print(y);
+  Serial.print(", z=");
+  Serial.println(z);
 }
